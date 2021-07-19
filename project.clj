6f7a306f27084b77c7822ba23748069ca765b076
Laverne Schrock <laverne@schrock.email>
2021-07-19 11:29:08 -0400

    Don't sign clojars releases

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject org.clojars.lverns/demo-lib "0.1.0"
   :description "A demo library"
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns demo-lib.core})
+  :repl-options {:init-ns demo-lib.core}
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org"
+                                     :sign-releases false}]
+                        ["snapshots" :clojars]])
