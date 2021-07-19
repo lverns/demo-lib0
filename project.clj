e32bf54d39840dac65d73227762700f51bac2a86
Laverne Schrock <laverne@schrock.email>
2021-07-19 09:40:14 -0400

    Make non-snapshot release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lverns/demo-lib "0.1.0-SNAPSHOT"
+(defproject org.clojars.lverns/demo-lib "0.1.0"
   :description "A demo library"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns demo-lib.core})
